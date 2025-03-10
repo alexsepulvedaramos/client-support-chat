ededc021a311ecf876c92ea8a2c82a3f9a62ddc3
Fernando Herrera <fernando.herrera85@gmail.com>
2025-03-10 15:16:17 -0400

    Add Radix UI ScrollArea and initial chat layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { AuthLayout } from './auth/layout/AuthLayout';
-
+import ChatLayout from './chat/layout/ChatLayout';
+import ChatPage from './chat/pages/ChatPage';
 function App() {
   return (
     <>
-      <AuthLayout />
+      {/* <AuthLayout /> */}
+      <ChatLayout>
+        <ChatPage />
+      </ChatLayout>
     </>
   );
 }
